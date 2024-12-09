9340d522f6d54be9339d66b678dd53384498d484
Louka Houlgatte--Bustamante <louka.houlgatte-bustamante@doctolib.com>
2024-12-09 12:28:18 +0100

    day 7

---
+++ src/2024/day-7/part2.ts
@@ -0,0 +1,40 @@
+import fs from "node:fs";
+
+const main = () => {
+  const lines = getFileContent()
+
+  const result = lines.reduce((sum, line) => {
+    const [result, numbers] = line.split(':')
+    if (!numbers) return sum
+    const expectedResult = parseInt(result)
+    const parsedNumbers = numbers.trim().split(' ').map(n => parseInt(n))
+
+    if (isLineValid(expectedResult, parsedNumbers)) sum += expectedResult
+    return sum
+  }, 0)
+
+  console.log(result)
+}
+
+const isLineValid = (result: number, numbers: number[]): boolean => {
+  return canAchieveResult(result, numbers, 0, 0);
+}
+
+const canAchieveResult = (result: number, numbers: number[], index: number, current: number): boolean => {
+  if (index === numbers.length) {
+    return current === result;
+  }
+
+  const newIndex = index + 1;
+
+  return canAchieveResult(result, numbers, newIndex, current + numbers[index]) ||
+    canAchieveResult(result, numbers, newIndex, current * numbers[index]) ||
+    canAchieveResult(result, numbers, newIndex, parseInt(current + '' + numbers[index]));
+
+}
+
+const getFileContent = (): string[] => {
+  return fs.readFileSync('input.txt', 'utf8').toString().split('\n')
+}
+
+main()
