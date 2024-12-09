a5a04b9b4b341fcb4274af5cd717b650da66ee23
Louka Houlgatte--Bustamante <louka.houlgatte-bustamante@doctolib.com>
2024-12-09 14:56:42 +0100

    day 5 part 1

--- src/2024/day-5/part1.ts
+++ src/2024/day-5/part1.ts
@@ -1,28 +1,60 @@
 import fs from "node:fs";
 
+type Instructions = Record<string, Record<'before'|'after', number[]>>
+
 const main = () => {
   const file = getFileContent()
   const splitByType = file.split('SECTION')
   const instructions = splitByType[0].split('\n')
   const computedInstructions = computeInstructions(instructions)
+
   const cases = splitByType[1].split('\n')
 
   const result = cases.reduce((sum, currentCase) => {
     const parsedCase = currentCase.split(',').map(item => parseInt(item))
+    if(parsedCase.length === 1) return sum
     if (validCase(parsedCase, computedInstructions)) {
-      sum+= parseInt(currentCase[currentCase.length / 2])
+      sum+= parsedCase[Math.floor(parsedCase.length / 2)]
     }
     return sum
   }, 0)
   console.log(result)
 }
 
-const computeInstructions = (instructions: string[]): string[] => {
- return []
+const computeInstructions = (instructions: string[]) => {
+  return instructions.reduce<Instructions>((computedInstructions, curr) => {
+    const [first, second] = curr.split('|')
+    const parsedFirst = parseInt(first)
+    const parsedSecond = parseInt(second)
+    if (parsedFirst === parsedSecond) return computedInstructions
+    computedInstructions[first] = {
+      ...computedInstructions[first],
+      before: (computedInstructions[first]?.before ?? []).concat(parsedSecond)
+    }
+    computedInstructions[second] = {
+      ...computedInstructions[second],
+      after: (computedInstructions[second]?.after ?? []).concat(parsedFirst)
+    }
+    return computedInstructions
+  }, {})
 }
 
-const validCase = (parsedCase: number[], instructions: string[]): boolean => {
-  return true
+
+const validCase = (parsedCase: number[], instructions: Instructions): boolean => {
+  let valid = true
+  let index = 0
+  do {
+    const {before, after} = instructions[parsedCase[index]] ?? {}
+    const resultBefore = (before ?? []).map((currentNb) => parsedCase.indexOf(currentNb)).filter(currIndex => currIndex < index && currIndex !== -1)
+    console.log(after)
+    const resultAfter = (after ?? []).map((currentNb) => parsedCase.indexOf(currentNb)).filter(currIndex => currIndex > index && currIndex !== -1)
+    console.log(resultBefore, resultAfter)
+    if (resultBefore.length !== 0 || resultAfter.length !== 0) {
+      valid = false
+    }
+    index++
+  } while(valid && index < parsedCase.length)
+  return valid
 }
 
 const getFileContent = (): string => {
