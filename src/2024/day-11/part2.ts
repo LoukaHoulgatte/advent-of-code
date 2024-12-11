4e557713ffdaaddf27c33eac546455c4dd74c8a8
Louka Houlgatte--Bustamante <louka.houlgatte-bustamante@doctolib.com>
2024-12-11 11:48:09 +0100

    day 11 part 2 - introduce memoization

--- src/2024/day-11/part2.ts
+++ src/2024/day-11/part2.ts
@@ -4,10 +4,11 @@ const main = () => {
   const file = getFileContent()
   let initialNumbers = file.split(' ')
 
+  const memo: Record<string, number> = {};
 
   const computed = initialNumbers.map((num, i) => {
     console.log('------------------------------------------' + i)
-    return processNum(num, 0, 75)
+    return processNum(num, 0, 75, memo)
   })
   console.log(computed.reduce((sum, curr) => {
     sum+= curr;
@@ -15,21 +16,32 @@ const main = () => {
   } ))
 }
 
-const processNum = (num: string, index: number, max: number): number => {
+const processNum = (num: string, index: number, max: number, memo: Record<string, number> ): number => {
+  const key = `${num}-${index}`;
+  if (memo[key] !== undefined) {
+    return memo[key];
+  }
   if (index === max) {
     return 1
   }
 
   const newIndex = index + 1
-  if(num === '0') return processNum('1', newIndex, max)
-  if(num.length %2 === 0) {
+  let result: number;
+
+  if(num === '0') {
+    result = processNum('1', newIndex, max, memo)
+  } else if(num.length %2 === 0) {
     const parsedNum = num.split('')
     const firstPart = parsedNum.slice(0, parsedNum.length / 2).join('')
     const secondPart = parsedNum.slice(parsedNum.length / 2).join('')
 
-    return processNum(parseInt(firstPart) + '', newIndex, max) + processNum(parseInt(secondPart) + '', newIndex, max)
+    result = processNum(parseInt(firstPart) + '', newIndex, max, memo) + processNum(parseInt(secondPart) + '', newIndex, max, memo)
+  } else {
+    result = processNum(parseInt(num)*2024 + '', newIndex, max, memo)
   }
-  return processNum(parseInt(num)*2024 + '', newIndex, max)
+
+  memo[key] = result;
+  return result;
 }
 
 const getFileContent = (): string => {
